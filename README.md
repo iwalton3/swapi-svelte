3083264a7114c2e5d38a078c32e7b87c4f7063d6
Ian Walton <iwalton3@gmail.com>
2019-08-27 13:57:46 -0400

    Add material UI.

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ For those who are unaware of Svelte, It is easy to learn web framework, packed w
 
 To build the project, you'll need an swapi server. Download the `api.js` from your server by appending `.js` to the end of your API url. Run `npm install` to download the build dependencies. Then use `npm run build` to build. For development, use `npm run dev`.
 
+## Material UI
+
+This repository contains all the dependencies and build configurations to use Material UI with svelte.
+
 ## SCSS Support
 
 You can use scss files by including them from main.js globally or by using `lang="sass"` in a svelte component. Please note that scss in svelte components doesn't play nice with the Visual Studio Code svelte extension.
--- src/Home.svelte
+++ src/Home.svelte
@@ -2,9 +2,33 @@
     import Page from './router/Page.svelte';
     import AppList from './router/AppList.svelte';
     import UserTools from './auth/UserTools.svelte';
+
+    import TopAppBar, {Row, Section, Title} from '@svelte-material-ui/top-app-bar';
+    import IconButton from '@svelte-material-ui/icon-button';
+    function doSomething() {
+        alert('something');
+    }
+    let prominent = false;
+    let dense = false;
+    let secondaryColor = false;
+    let clicked = 0;
 </script>
 
 <Page>
+<TopAppBar variant="static"  {prominent} {dense} color={secondaryColor ? 'secondary' : 'primary'}>
+    <Row>
+        <Section>
+            <IconButton class="material-icons">menu</IconButton>
+            <Title>Static</Title>
+         </Section>
+         <Section align="end" toolbar>
+            <IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+            <IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+            <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+          </Section>
+    </Row>
+</TopAppBar>
+<IconButton class="material-icons" on:click={() => clicked++} ripple={false}>add_shopping_cart</IconButton> (no ripple)
     <AppList/>
     <UserTools/>
 </Page>
