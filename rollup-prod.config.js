fcb40a07d8d03cf2a5c36d67df17021e82f07192
Ian Walton <ian@iwalton.com>
2020-03-05 19:36:17 -0500

    Switch to @rollup/plugin-node-resolve.

--- rollup-prod.config.js
+++ rollup-prod.config.js
@@ -1,5 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
